42956bf4d44c0240b48d0ed5c39cecb6c7cbd4a1
Jacob Carver <jake10693@gmail.com>
2020-06-06 11:03:55 -0700

    changed PORT for launch

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect(process.env.MONGODB_URI || "mongodb:jake10693:thissucks1D!.mlab.com:11082/heroku_9mp1g9d7", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
